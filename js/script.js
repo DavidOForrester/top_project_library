f052e5689fbf77091c17503d89002bd8e24f858e
David Forrester <david.o.forrester@gmail.com>
2023-05-07 09:08:16 +1000

    added button to open form for new book entry

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -10,25 +8,6 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
-
-const form = document.getElementById("book-form");
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const formData = new FormData(form);
-  const title = formData.get("title");
-  const author = formData.get("author");
-  const pages = formData.get("pages");
-  const read = formData.get("read");
-
-  let book = new Book(title, author, pages, read);
-
-  addBookToLibrary(book);
-
-  updateTable();
-
-  console.log(myLibrary);
-});
 
 function updateTable() {
   const tableBody = document.getElementById("table-body");
@@ -48,3 +27,39 @@ function updateTable() {
     readCell.innerText = book.read;
   }
 }
+
+function openForm() {
+  let form = document.getElementById("book-form");
+  form.style.display = "block";
+}
+
+//Cancel button here
+function cancelForm() {}
+
+let myLibrary = [];
+
+const form = document.getElementById("book-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const pages = formData.get("pages");
+  const read = formData.get("read");
+
+  let book = new Book(title, author, pages, read);
+
+  addBookToLibrary(book);
+
+  updateTable();
+
+  var inputs = form.querySelectorAll("input");
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].value = "";
+  }
+
+  form.style.display = "none";
+
+  console.log(myLibrary);
+});

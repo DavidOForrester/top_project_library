899a7965abba806416bc47a6a8b66481fc839e11
David Forrester <david.o.forrester@gmail.com>
2023-05-06 13:44:23 +1000

    js changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-// Variables
+let myLibrary = [];
 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
-// Code body
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
+const book = new Book("Test Title", "Test Author", 1, true);
 
-// Functions
+addBookToLibrary(book);
 
+const tableBody = document.getElementById("table-body");
+for (let book of myLibrary) {
+  const row = tableBody.insertRow();
+  const titleCell = row.insertCell();
+  const authorCell = row.insertCell();
+  const pagesCell = row.insertCell();
+  const readCell = row.insertCell();
+
+  titleCell.innerText = book.title;
+  authorCell.innerText = book.author;
+  pagesCell.innerText = book.pages;
+  readCell.innerText = book.read;
+}
+
+console.log(myLibrary);

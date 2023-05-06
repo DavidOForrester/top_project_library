77654e11e1b0f30532a0d92b7abf967e9e90db0a
David Forrester <david.o.forrester@gmail.com>
2023-05-06 13:56:59 +1000

    form and table updates removing rows before update sourcing data from form and storing into array

--- js/script.js
+++ js/script.js
@@ -11,22 +11,40 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-const book = new Book("Test Title", "Test Author", 1, true);
+const form = document.getElementById("book-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-addBookToLibrary(book);
+  const formData = new FormData(form);
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const pages = formData.get("pages");
+  const read = formData.get("read");
 
-const tableBody = document.getElementById("table-body");
-for (let book of myLibrary) {
-  const row = tableBody.insertRow();
-  const titleCell = row.insertCell();
-  const authorCell = row.insertCell();
-  const pagesCell = row.insertCell();
-  const readCell = row.insertCell();
+  let book = new Book(title, author, pages, read);
 
-  titleCell.innerText = book.title;
-  authorCell.innerText = book.author;
-  pagesCell.innerText = book.pages;
-  readCell.innerText = book.read;
+  addBookToLibrary(book);
+
+  updateTable();
+
+  console.log(myLibrary);
+});
+
+function updateTable() {
+  const tableBody = document.getElementById("table-body");
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+  for (let book of myLibrary) {
+    const row = tableBody.insertRow();
+    const titleCell = row.insertCell();
+    const authorCell = row.insertCell();
+    const pagesCell = row.insertCell();
+    const readCell = row.insertCell();
+
+    titleCell.innerText = book.title;
+    authorCell.innerText = book.author;
+    pagesCell.innerText = book.pages;
+    readCell.innerText = book.read;
+  }
 }
-
-console.log(myLibrary);
